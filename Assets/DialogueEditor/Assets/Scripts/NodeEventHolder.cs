bf26fc033b4983667e2ab8ed6f2c4e732a816e63
Mikkel <58807701+professorcrazy@users.noreply.github.com>
2025-02-04 15:54:19 +0100

    Initial Setup

---
+++ Assets/DialogueEditor/Assets/Scripts/NodeEventHolder.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+namespace DialogueEditor
+{
+    /// <summary>
+    /// This class holds all of the values for a node which 
+    /// need to be serialized. 
+    /// </summary>
+    [System.Serializable]
+    public class NodeEventHolder : MonoBehaviour
+    {
+        [SerializeField] public UnityEngine.Events.UnityEvent Event;
+
+        [SerializeField] public int NodeID;
+        [SerializeField] public TMPro.TMP_FontAsset TMPFont;
+        [SerializeField] public Sprite Icon;
+        [SerializeField] public VideoClip video;
+    }
+}
